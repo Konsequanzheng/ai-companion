e0ef7d1d58f5fbb6435c2cc7928b7c438ddb0fbb
Quan Zheng <q.zheng-7@student.tudelft.nl>
2023-11-04 14:34:18 +0200

    Add stripe backend

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,13 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+// returns the url that the application is currently running on
+export function absoluteUrl(path: string) {
+  return `${process.env.NEXT_PUBLIC_APP_URL}${path}`;
+}
+
+absoluteUrl("settings"); // http://localhost:3000/settings
